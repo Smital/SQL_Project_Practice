d1ed58cb018acc5ce0c3046f6cd166c32c3bbc61
Smital Christian <spchristian44@gmail.com>
2026-05-30 21:31:09 -0400

    push example

--- sql_load/3_modify_tables.sql
+++ sql_load/3_modify_tables.sql
@@ -26,19 +26,19 @@ NOTE: If you are having issues with permissions. And you get error:
 -- NOTE: This has been updated from the video to fix issues with encoding
 
 COPY company_dim
-FROM 'E:\SQL_Projects\csv_files\company_dim.csv'
+FROM 'E:\SQL\SQL_Project_Practice\csv_files\company_dim.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 COPY skills_dim
-FROM 'E:\SQL_Projects\csv_files\skills_dim.csv'
+FROM 'E:\SQL\SQL_Project_Practice\csv_files\skills_dim.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 COPY job_postings_fact
-FROM 'E:\SQL_Projects\csv_files\job_postings_fact.csv'
+FROM 'E:\SQL\SQL_Project_Practice\csv_files\job_postings_fact.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 COPY skills_job_dim
-FROM 'E:\SQL_Projects\csv_files\skills_job_dim.csv'
+FROM 'E:\SQL\SQL_Project_Practice\csv_files\skills_job_dim.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 
